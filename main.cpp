39166acae54aa65ad40cbfdeca1eeecce0b4ea67
23005291 <163582927+23005291@users.noreply.github.com>
2024-04-18 20:42:36 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,27 +4,48 @@ using namespace std;
 
 int main()
 {
-    double totalCost, employeeSalary, yearlyRent, electricityCost;
+    int age, Contender_Age,laugh, Height, money;
+    char gender;
 
-    cout<<"Enter the total cost of the merchandise: ";
-    cin>>totalCost;
+    cout<< "Enter your gender (male or female) : "<<endl;
+    cin>> gender;
 
-    cout<<"Enter the salary of the employee: ";
-    cin>>employeeSalary;
+    cout << "Enter your age: "<<endl;
+    cin >> age;
 
-    cout<<"Enter the yearly rent: ";
-    cin>>yearlyRent;
+    cout << "Enter your potential partners age: "<<endl;
+    cin >>  Contender_Age;
 
-    cout<<"Enter the estimated electricity cost: ";
-    cin>>electricityCost;
+    cout << "Do want a person who makes you laugh? (1 for yes or 0 for no)"<<endl;
+    cin >>  laugh;
 
-    double totalExpenses = totalCost + employeeSalary + yearlyRent + electricityCost;
-    double desiredNetProfit =totalCost* 0.1;
-    double newNetProfit = desiredNetProfit/0.85;
-    double markUp = (newNetProfit+totalExpenses)/totalCost *100;
+    cout << "Do you want a tall or short person?( 1 for tall or 0 for short) "<<endl;
+    cin >>  Height;
 
-    cout<<"The merchandise should be marked up by "<< markUp<<"%"<<endl;
+    cout << "Should they be having money? (1 for yes or 0 for no)"<<endl;
+    cin >>  money;
+
+
+ if (gender == 'male') {
+        if (age >= 16 && age <= 25) {
+            if (laugh&& Height && money) {
+                cout << "shes the one for you " << endl;
+            } else {
+                cout << "Sorry, shes not the one " << endl;
+            }
+        }
+    } else if (gender == 'female') {
+        if (age >= 20 && age <= 35) {
+            if (laugh && Height && money) {
+                cout << " hes the one for you" << endl;
+            } else {
+                cout << "Sorry, hes not the one" << endl;
+            }
+        }
+    } else {
+        cout << "Invalid input! " << endl;
+    }
+
 
     return 0;
-
 }
